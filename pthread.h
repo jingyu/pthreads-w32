fcb805accf71399bad1ab471e5dccf0500616bc7
Jingyu <jingyu.niu@gmail.com>
2016-04-15 16:53:55 +0800

    Add HAVE_STRUCT_TIMESPEC definition for vs14

--- pthread.h
+++ pthread.h
@@ -313,6 +313,10 @@ enum {
 #endif
 #endif
 
+#if _MSC_VER >= 1900
+#define HAVE_STRUCT_TIMESPEC
+#endif
+
 #if !defined(HAVE_STRUCT_TIMESPEC)
 #define HAVE_STRUCT_TIMESPEC
 #if !defined(_TIMESPEC_DEFINED)
